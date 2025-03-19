b81a6535b2c21d514cd9c86d46b7bf72f39f1759
Ruslan B <rusbez04@yandex.ru>
2025-03-19 12:29:29 +0500

    Добавлен интерфейс для приложения

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtCore import Qt
+
+import sys
+
+
 class CCircle:
     def __init__(self, x, y, radius=30):
         self.x = x
@@ -14,3 +20,17 @@ class Storage:
         self.__data.remove(obj)
     def get_all(self):
         return self.__data[:]
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Рисуем круги")
+        self.setGeometry(300, 300, 640, 480)
+        self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec())
